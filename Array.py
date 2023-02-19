81778e28c44eb6596517bb4770467797efd57115
Mahdi Hasan Bhuiyan <109209066+Mahdi0082@users.noreply.github.com>
2023-02-19 19:36:40 +0600

    Update Array.py

--- Array.py
+++ Array.py
@@ -1,3 +1,11 @@
+#Consider an array named source. Write a method/function named shiftLeft( source, k) that shifts all the elements of the source array to the left by 'k' positions. You must execute the method by passing an array and number of cells to be shifted. After calling the method, print the array to show whether the elements have been shifted properly.
+#Example: source=[10,20,30,40,50,60]
+#shiftLeft(source,3)
+#After calling shiftLeft(source,3), printing the array should give the output as: 
+# [ 40, 50, 60, 0, 0, 0 ]
+
+
+
 source=[40,30,2,54,123,21,23]
 
 def leftshift(source,k):
